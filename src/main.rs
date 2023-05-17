e557fe01abc37c1b1bc718cbf7fd4ce50ab629e6
叶计义 <horaoen@gmail.com>
2023-05-17 09:41:55 +0800

    test diff cached

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use mini_redis::{client, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Open a connection to the mini-redis address.
     let mut client = client::connect("127.0.0.1:6379").await?;
 
     // Set the key "hello" with value "world"
